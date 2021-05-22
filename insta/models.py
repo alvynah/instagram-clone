862c2962357582a8a4a75038d41fab9f31eb373c
alvynah <alvynahw@gmail.com>
2021-05-22 06:45:28 +0300

    Add model for post

--- insta/models.py
+++ insta/models.py
@@ -8,3 +8,10 @@ class Profile(models.Model):
     bio=models.TextField(max_length=500,blank=True)
     profile_pic=models.ImageField(upload_to='pictures/')
 
+class Post(models.Model):
+    image = models.ImageField(upload_to='posts/')
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=250, blank=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
